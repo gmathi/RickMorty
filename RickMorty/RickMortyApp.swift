8036a97fed02bc213519553878faccc3d2bfa84c
Govardhan Naidu Mathi <gmathi.developer@gmail.com>
2026-01-28 21:29:15 -0600

    Initial Commit

---
+++ RickMorty/RickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApp.swift
+//  RickMorty
+//
+//  Created by Govardhan Mathi on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
